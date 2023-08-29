371ffd5034f39d5c01d72f690813505d1f0bf859
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2023-08-29 10:46:13 -0300

    Update Exercicio4.c

--- Exercicio4.c
+++ Exercicio4.c
@@ -7,10 +7,10 @@ main(){
     printf("Digite seu peso em kg: ");
     scanf("%f", &peso);
 
-    if (peso >= 60) {
-        printf("Peso maior ou igual a 60kg");
-    } else {
+    if (peso < 60) {
         printf("Peso menor que 60kg");
+    } else {
+        printf("Peso maio ou igual a 60kg");
     }
 
 
